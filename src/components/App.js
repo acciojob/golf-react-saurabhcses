92720b9177b7b6be89ca041217662cef33912717
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-10-31 09:43:42 +0530

    Refactor App component for cleaner state management

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,50 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      ballPosition: { left: "0px" },
     };
+    this.renderBallOrButton = this.renderBallOrButton.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+  }
 
-    buttonClickHandler() {
-   this.setState({ renderBall: true });
-      document.addEventListener("keydown",(event)=>{
-        if (event.keyCode === 39) {
-      
+  buttonClickHandler() {
+    this.setState({ renderBall: true });
+
+    // Add keydown listener once ball appears
+    document.addEventListener("keydown", (event) => {
+      if (event.keyCode === 39) {
+        // Move right by 5px
         this.setState((prevState) => {
           const currentLeft = parseInt(prevState.ballPosition.left, 10);
           return {
-            ballPosition: { left: currentLeft + 5 + "px" }
+            ballPosition: { left: currentLeft + 5 + "px" },
           };
         });
       }
-      })
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
+    });
+  }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
+  renderBallOrButton() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else {
+      // Make sure button has class="start"
+      return (
+        <button className="start" onClick={this.buttonClickHandler}>
+          Start
+        </button>
+      );
     }
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
+  render() {
+    return <div className="playground">{this.renderBallOrButton()}</div>;
+  }
 }
-
 
 export default App;
